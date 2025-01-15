0b55b249e50c2691efef2853016ed0dc55611294
Lucas Barbosa Antonelli <lucaslba2105@gmail.com>
2025-01-14 23:46:37 -0300

    deploy

---
+++ src/main/java/com/apitodo/todolist/task/TaskController.java
@@ -0,0 +1,55 @@
+package com.apitodo.todolist.task;
+
+
+import com.apitodo.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) ||currentDate.isAfter(taskModel.getEndAt()) ){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / término deve ser maior do que a data atual");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início  deve ser menor do que a data de término");
+        }
+        var task = this.taskRepository.save(taskModel);
+        return  ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasksList =  this.taskRepository.findByIdUser((UUID) idUser);
+        return tasksList;
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id,  HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var task = this.taskRepository.findById(id).orElse(null);
+        if(task == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        if(!idUser.equals(task.getIdUser()))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não é o dono da tarefa");
+        Utils.copyNonNullProperties(taskModel,task);
+        var taskUpdated =  this.taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(taskUpdated);
+    }
+
+}
